6be918979964b2ad6d42e19c25341a0ea739260c
JustForUniversity <55394934+JustForUniversity@users.noreply.github.com>
2022-11-29 18:19:09 +0300

    1

---
+++ detect_model/detect.bat
@@ -0,0 +1,5 @@
+cd yolo
+python detect.py --source ../img --project ../result --weights ../weights/best.pt --name=detection --img 256 --conf 0.4 --line-thickness 1 --save-txt --save-conf
+
+/python detect.py --source ../img --project ../result --weights ../weights/best.pt --img 256 --conf 0.4 --line-thickness 1
+
