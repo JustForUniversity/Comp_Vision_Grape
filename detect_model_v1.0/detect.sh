26ef62bde144642581419b415c7d6b24abc5374a
JustForUniversity <55394934+JustForUniversity@users.noreply.github.com>
2022-12-20 12:00:51 +0300

    fix shell script_v1
    fix shell script

--- detect_model_v1.0/detect.sh
+++ detect_model_v1.0/detect.sh
@@ -2,4 +2,4 @@
 # Detect shell script
 
 cd yolo
-python detect.py --source ../img --project ../result --weights ../weights/best.pt --name=detection --img 256 --conf 0.4 --line-thickness 1 --save-txt --save-conf
+python3 detect.py --source ../img --project ../result --weights ../weights/best.pt --name=detection --img 256 --conf 0.4 --line-thickness 1 --save-txt --save-conf
